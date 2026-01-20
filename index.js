cbf9bd818fcd5ca4f824a09928749b5a1accd3ec
Goodman-code64 <jeeneshrajput189@gmail.com>
2026-01-20 12:47:20 +0530

    Day 2: Learn variables, const vs let, and memory behavior

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 console.log('hello world')
 console.log("this code is running using node")
+// Day 2 practice 
+
+const name ="jeenesh";
+let year ='2026';
+console.log("Name:",name);
+console.log("Year",year);
+year=2027;
+console.log("updated year",year);
+//primitive example 
+let a=5;
+ let b=a;
+ b=10;
+
+ console.log("a:",a)
+ console.log("b:",b)
+//refernce example 
+const user={
+  role:"student",
+};
+const sameuser= user;
+sameuser.role="backend engineer";
+console.log("user role:", user.role)
+
+
